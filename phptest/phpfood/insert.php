b6f1921846072c180c2f6aa8e07ce059572da888
young199227 <68836965+young199227@users.noreply.github.com>
2022-12-13 16:27:26 +0800

    老師的題目

---
+++ phptest/phpfood/insert.php
@@ -0,0 +1,58 @@
+<?php
+
+//{"pname":"超好吃飯","price":"100","pnum":"1"}
+
+$data = file_get_contents("php://input","r");
+
+$jsonData = array();
+
+$jsonData = json_decode($data, true);
+
+if(isset($jsonData["pname"])&&isset($jsonData["price"])&&isset($jsonData["pnum"])){
+
+    if($jsonData["pname"]!=""&&$jsonData["price"]!=""&&$jsonData["pnum"]){
+
+        $p_Pname = $jsonData["pname"];
+        $p_Price = $jsonData["price"];
+        $p_Pnum = $jsonData["pnum"];
+        
+
+        $servername = "localhost";
+        $username = "owner";
+        $password = "123456";
+        $dbname = "testdb";
+        
+        $conn = mysqli_connect($servername,$username,$password,$dbname);
+        
+        if(!$conn){
+            die("連線錯誤:".mysqli_connect_error());
+        }
+        
+        $sql = "INSERT INTO food01(Pname,Price,Pnum) VALUES ($p_Pname,$p_Price,$p_Pnum)";
+        
+
+        if(mysqli_query($conn,$sql)){
+
+            echo '{"state":"true","message":"新增成功"}';
+        }else{
+
+            echo '{"state":"false","message":"新增資料失敗"'.$sql.mysqli_error($conn).'}';
+        }
+
+        mysqli_close($conn);
+
+    }else{
+
+        echo '{"state":"false","message":"欄位有空值新增失敗"}';
+    }
+
+}else{
+
+    echo '{"state":"false","message":"缺少欄位新增失敗"}';
+}
+
+
+
+
+
+?>
--- testdb.sql
+++ testdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： localhost
--- 產生時間： 2022 年 12 月 06 日 16:30
+-- 產生時間： 2022 年 12 月 13 日 16:26
 -- 伺服器版本： 8.0.31-0ubuntu0.22.04.1
 -- PHP 版本： 7.4.32
 
@@ -25,9 +25,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `food01`
+--
+
+DROP TABLE IF EXISTS `food01`;
+CREATE TABLE `food01` (
+  `ID` int NOT NULL COMMENT '編號',
+  `Pname` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '便當名稱',
+  `Price` int NOT NULL COMMENT '單價',
+  `Pnum` int NOT NULL COMMENT '數量',
+  `Created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '建單日期'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `food01`
+--
+
+INSERT INTO `food01` (`ID`, `Pname`, `Price`, `Pnum`, `Created_at`) VALUES
+(1, '雞腿飯', 90, 1, '2022-12-13 07:19:02'),
+(2, '雞腿飯', 100, 1, '2022-12-13 07:33:42');
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `product`
 --
 
+DROP TABLE IF EXISTS `product`;
 CREATE TABLE `product` (
   `ID` int NOT NULL COMMENT '編號',
   `Pname` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '產品名稱',
@@ -51,10 +75,11 @@ INSERT INTO `product` (`ID`, `Pname`, `Price`, `Pnum`, `Created_at`) VALUES
 -- 資料表結構 `user01`
 --
 
+DROP TABLE IF EXISTS `user01`;
 CREATE TABLE `user01` (
   `ID` int NOT NULL,
   `Username` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Password` int NOT NULL,
+  `Password` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
   `Nickname` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
   `Height` int NOT NULL,
   `Weight` int NOT NULL,
@@ -66,29 +91,28 @@ CREATE TABLE `user01` (
 --
 
 INSERT INTO `user01` (`ID`, `Username`, `Password`, `Nickname`, `Height`, `Weight`, `Created_at`) VALUES
-(1, 'ggyy', 123456, 'gy', 170, 110, '2022-12-06 07:22:06'),
-(2, 'Capybara', 123456, '水豚君', 50, 50, '2022-12-06 07:23:03'),
-(3, 'turtle', 123456, '烏龜', 50, 250, '2022-12-06 07:23:42'),
-(4, '❤❤❤', 123456, '佳佳', 160, 8787, '2022-12-06 07:52:34'),
-(5, '假帳號', 8787, '假暱稱', 100, 100, '2022-12-06 07:52:41'),
-(6, '假帳號', 8787, '假暱稱', 100, 100, '2022-12-06 07:52:47'),
-(7, '假帳號', 8787, '假暱稱', 100, 100, '2022-12-06 07:52:52'),
-(8, '假帳號', 8787, '假暱稱', 100, 100, '2022-12-06 07:52:55'),
-(9, '假帳號', 8787, '假暱稱', 100, 100, '2022-12-06 07:52:56'),
-(10, '假帳號', 8787, '假暱稱', 100, 100, '2022-12-06 07:52:58'),
-(11, '假帳號', 8787, '假暱稱', 100, 100, '2022-12-06 07:53:00'),
-(12, '假帳號', 8787, '假暱稱', 100, 100, '2022-12-06 07:53:01'),
-(13, '假帳號', 8787, '假暱稱', 100, 100, '2022-12-06 07:53:03'),
-(14, '假帳號', 8787, '假暱稱', 100, 100, '2022-12-06 07:53:05'),
-(15, '假帳號', 8787, '假暱稱', 100, 100, '2022-12-06 07:53:07'),
-(16, '假帳號', 8787, '假暱稱', 100, 100, '2022-12-06 07:53:09'),
-(17, '假帳號', 8787, '假暱稱', 100, 100, '2022-12-06 07:53:14'),
-(18, '假帳號', 8787, '假暱稱', 100, 100, '2022-12-06 07:53:16'),
-(19, '假帳號', 8787, '假暱稱', 100, 100, '2022-12-06 07:53:18');
+(65, '123', '123123', '2222', 22222, 222, '2022-12-13 01:34:25'),
+(66, 'XXX', 'XXX', 'xxx', 123, 100, '2022-12-13 03:44:41'),
+(67, '123', 'XXX', 'xxx', 123, 100, '2022-12-13 05:16:29'),
+(68, '123213', '123213213', '12321', 111, 7, '2022-12-13 05:43:10'),
+(69, '123213', '123213213', '12321', 111, 7, '2022-12-13 05:43:36'),
+(70, '123213', '123213213', '12321', 111, 7, '2022-12-13 05:44:19'),
+(71, '123213', '123213213', '12321', 111, 7, '2022-12-13 05:44:20'),
+(72, '123213', '123213213', '12321', 111, 7, '2022-12-13 05:44:21'),
+(73, '123213', '123213213', '12321', 111, 7, '2022-12-13 05:44:21'),
+(74, '12354124', '12321222', '12321', 111, 7, '2022-12-13 05:44:54'),
+(75, '12312321', '123212222', '11223', 111, 90, '2022-12-13 05:46:49'),
+(76, '123123213', '32131231232', '12312321', 111, 87, '2022-12-13 05:59:55');
 
 --
 -- 已傾印資料表的索引
 --
+
+--
+-- 資料表索引 `food01`
+--
+ALTER TABLE `food01`
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- 資料表索引 `product`
@@ -107,6 +131,12 @@ ALTER TABLE `user01`
 --
 
 --
+-- 使用資料表自動遞增(AUTO_INCREMENT) `food01`
+--
+ALTER TABLE `food01`
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT COMMENT '編號', AUTO_INCREMENT=12;
+
+--
 -- 使用資料表自動遞增(AUTO_INCREMENT) `product`
 --
 ALTER TABLE `product`
@@ -116,7 +146,7 @@ ALTER TABLE `product`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `user01`
 --
 ALTER TABLE `user01`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

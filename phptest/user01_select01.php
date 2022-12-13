6098e23f30d5488533e15fb5ffb04b13d057252c
young199227 <68836965+young199227@users.noreply.github.com>
2022-12-13 13:57:08 +0800

    超小改

--- phptest/user01_select01.php
+++ phptest/user01_select01.php
@@ -13,6 +13,8 @@
 
     $sql = "SELECT * FROM user01 ORDER BY ID";
 
+    $result = mysqli_query($conn,$sql);
+
     if(mysqli_num_rows($result)>0){
 
         while($row = mysqli_fetch_assoc($result)){
